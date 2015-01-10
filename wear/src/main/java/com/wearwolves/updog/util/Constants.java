bb16f5e5440482fdfbe7e4604dbf79f59e1c65f5
Adam Dziki <adam.dziki+gh@gmail.com>
2015-01-10 13:00:41 -0500

    initial mods to support Station -> Stop relationship

---
+++ wear/src/main/java/com/wearwolves/updog/util/Constants.java
@@ -0,0 +1,13 @@
+package com.wearwolves.updog.util;
+
+/**
+ * Created by adam on 1/10/15.
+ */
+public class Constants {
+
+    public final static String LOCUS_REDLINE = "Park Street";
+    public final static String LOCUS_GREENLINE = "Government Center";
+    public final static String LOCUS_BLUELINE = "Government Center";
+    public final static String LOCUS_ORANGELINE = "Downtown Crossing";
+    public final static String LOCUS_SILVERLINE = "South Station";
+}
