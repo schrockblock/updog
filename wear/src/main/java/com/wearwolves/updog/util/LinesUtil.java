cdfa75f0d2dedfcca369c9b8a41ab8201b85d41e
Adam Dziki <adam.dziki+gh@gmail.com>
2015-01-10 11:59:58 -0500

    removed tabs and lines

--- wear/src/main/java/com/wearwolves/updog/util/LinesUtil.java
+++ wear/src/main/java/com/wearwolves/updog/util/LinesUtil.java
@@ -11,104 +11,104 @@ import java.util.HashMap;
  */
 public class LinesUtil {
 
-    public static String hardcodedRed = "{\n" +
-            "\t\"name\" : \"Red Line\",\n" +
-            "\t\"identifier\" : \"redline\",\n" +
-            "\t\"variant\" : \"\",\n" +
-            "\t\"shorthand\" : \"RL\",\n" +
-            "\t\"stops\" : \n" +
-            "\t[\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"1\",\n" +
-            "\t\t\t\"stop_id\": \"70061\",\n" +
-            "\t\t\t\"stop_name\": \"Alewife\",\n" +
-            "\t\t\t\"parent_station\": \"place-alfcl\",\n" +
-            "\t\t\t\"parent_station_name\": \"Alewife\",\n" +
-            "\t\t\t\"stop_lat\": \"42.395428\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.142483\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"2\",\n" +
-            "\t\t\t\"stop_id\": \"70063\",\n" +
-            "\t\t\t\"stop_name\": \"Davis\",\n" +
-            "\t\t\t\"parent_station\": \"place-davis\",\n" +
-            "\t\t\t\"parent_station_name\": \"Davis\",\n" +
-            "\t\t\t\"stop_lat\": \"42.39674\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.121815\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"3\",\n" +
-            "\t\t\t\"stop_id\": \"70065\",\n" +
-            "\t\t\t\"stop_name\": \"Porter\",\n" +
-            "\t\t\t\"parent_station\": \"place-portr\",\n" +
-            "\t\t\t\"parent_station_name\": \"Porter\",\n" +
-            "\t\t\t\"stop_lat\": \"42.3884\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.119149\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"4\",\n" +
-            "\t\t\t\"stop_id\": \"70067\",\n" +
-            "\t\t\t\"stop_name\": \"Harvard\",\n" +
-            "\t\t\t\"parent_station\": \"place-harsq\",\n" +
-            "\t\t\t\"parent_station_name\": \"Harvard\",\n" +
-            "\t\t\t\"stop_lat\": \"42.373362\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.118956\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"5\",\n" +
-            "\t\t\t\"stop_id\": \"70069\",\n" +
-            "\t\t\t\"stop_name\": \"Central\",\n" +
-            "\t\t\t\"parent_station\": \"place-cntsq\",\n" +
-            "\t\t\t\"parent_station_name\": \"Central\",\n" +
-            "\t\t\t\"stop_lat\": \"42.365486\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.103802\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"6\",\n" +
-            "\t\t\t\"stop_id\": \"70071\",\n" +
-            "\t\t\t\"stop_name\": \"Kendall/MIT\",\n" +
-            "\t\t\t\"parent_station\": \"place-knncl\",\n" +
-            "\t\t\t\"parent_station_name\": \"Kendall/MIT\",\n" +
-            "\t\t\t\"stop_lat\": \"42.36249079\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.08617653\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"7\",\n" +
-            "\t\t\t\"stop_id\": \"70073\",\n" +
-            "\t\t\t\"stop_name\": \"Charles/MGH\",\n" +
-            "\t\t\t\"parent_station\": \"place-chmnl\",\n" +
-            "\t\t\t\"parent_station_name\": \"Charles/MGH\",\n" +
-            "\t\t\t\"stop_lat\": \"42.361166\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.070628\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"8\",\n" +
-            "\t\t\t\"stop_id\": \"70075\",\n" +
-            "\t\t\t\"stop_name\": \"Park Street\",\n" +
-            "\t\t\t\"parent_station\": \"place-pktrm\",\n" +
-            "\t\t\t\"parent_station_name\": \"Park Street\",\n" +
-            "\t\t\t\"stop_lat\": \"42.35639457\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.0624242\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"9\",\n" +
-            "\t\t\t\"stop_id\": \"70077\",\n" +
-            "\t\t\t\"stop_name\": \"Downtown Crossing\",\n" +
-            "\t\t\t\"parent_station\": \"place-dwnxg\",\n" +
-            "\t\t\t\"parent_station_name\": \"Downtown Crossing\",\n" +
-            "\t\t\t\"stop_lat\": \"42.355518\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.060225\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t\"stop_order\": \"10\",\n" +
-            "\t\t\t\"stop_id\": \"70079\",\n" +
-            "\t\t\t\"stop_name\": \"South Station\",\n" +
-            "\t\t\t\"parent_station\": \"place-sstat\",\n" +
-            "\t\t\t\"parent_station_name\": \"South Station\",\n" +
-            "\t\t\t\"stop_lat\": \"42.352271\",\n" +
-            "\t\t\t\"stop_lon\": \"-71.055242\"\n" +
-            "\t\t}\n" +
-            "\t]\n" +
+    public static String hardcodedRed = "{" +
+            "\"name\" : \"Red Line\"," +
+            "\"identifier\" : \"redline\"," +
+            "\"variant\" : \"\"," +
+            "\"shorthand\" : \"RL\"," +
+            "\"stops\" : " +
+            "[" +
+            "{" +
+            "\"stop_order\": \"1\"," +
+            "\"stop_id\": \"70061\"," +
+            "\"stop_name\": \"Alewife\"," +
+            "\"parent_station\": \"place-alfcl\"," +
+            "\"parent_station_name\": \"Alewife\"," +
+            "\"stop_lat\": \"42.395428\"," +
+            "\"stop_lon\": \"-71.142483\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"2\"," +
+            "\"stop_id\": \"70063\"," +
+            "\"stop_name\": \"Davis\"," +
+            "\"parent_station\": \"place-davis\"," +
+            "\"parent_station_name\": \"Davis\"," +
+            "\"stop_lat\": \"42.39674\"," +
+            "\"stop_lon\": \"-71.121815\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"3\"," +
+            "\"stop_id\": \"70065\"," +
+            "\"stop_name\": \"Porter\"," +
+            "\"parent_station\": \"place-portr\"," +
+            "\"parent_station_name\": \"Porter\"," +
+            "\"stop_lat\": \"42.3884\"," +
+            "\"stop_lon\": \"-71.119149\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"4\"," +
+            "\"stop_id\": \"70067\"," +
+            "\"stop_name\": \"Harvard\"," +
+            "\"parent_station\": \"place-harsq\"," +
+            "\"parent_station_name\": \"Harvard\"," +
+            "\"stop_lat\": \"42.373362\"," +
+            "\"stop_lon\": \"-71.118956\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"5\"," +
+            "\"stop_id\": \"70069\"," +
+            "\"stop_name\": \"Central\"," +
+            "\"parent_station\": \"place-cntsq\"," +
+            "\"parent_station_name\": \"Central\"," +
+            "\"stop_lat\": \"42.365486\"," +
+            "\"stop_lon\": \"-71.103802\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"6\"," +
+            "\"stop_id\": \"70071\"," +
+            "\"stop_name\": \"Kendall/MIT\"," +
+            "\"parent_station\": \"place-knncl\"," +
+            "\"parent_station_name\": \"Kendall/MIT\"," +
+            "\"stop_lat\": \"42.36249079\"," +
+            "\"stop_lon\": \"-71.08617653\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"7\"," +
+            "\"stop_id\": \"70073\"," +
+            "\"stop_name\": \"Charles/MGH\"," +
+            "\"parent_station\": \"place-chmnl\"," +
+            "\"parent_station_name\": \"Charles/MGH\"," +
+            "\"stop_lat\": \"42.361166\"," +
+            "\"stop_lon\": \"-71.070628\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"8\"," +
+            "\"stop_id\": \"70075\"," +
+            "\"stop_name\": \"Park Street\"," +
+            "\"parent_station\": \"place-pktrm\"," +
+            "\"parent_station_name\": \"Park Street\"," +
+            "\"stop_lat\": \"42.35639457\"," +
+            "\"stop_lon\": \"-71.0624242\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"9\"," +
+            "\"stop_id\": \"70077\"," +
+            "\"stop_name\": \"Downtown Crossing\"," +
+            "\"parent_station\": \"place-dwnxg\"," +
+            "\"parent_station_name\": \"Downtown Crossing\"," +
+            "\"stop_lat\": \"42.355518\"," +
+            "\"stop_lon\": \"-71.060225\"" +
+            "}," +
+            "{" +
+            "\"stop_order\": \"10\"," +
+            "\"stop_id\": \"70079\"," +
+            "\"stop_name\": \"South Station\"," +
+            "\"parent_station\": \"place-sstat\"," +
+            "\"parent_station_name\": \"South Station\"," +
+            "\"stop_lat\": \"42.352271\"," +
+            "\"stop_lon\": \"-71.055242\"" +
+            "}" +
+            "]" +
             "}";
     public static String hardcodedOrange = "";
 
